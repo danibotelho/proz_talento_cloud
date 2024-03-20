f4cb2c00bc0fed240c24d646d6fde4ea47119640
danibotelho <danibl26@gmail.com>
2024-03-20 13:10:08 -0300

    atividade for e while

---
+++ atividadeForWhile.py
@@ -0,0 +1,13 @@
+# Usando loop for
+print("FOR")
+for andar in range(20, 0, -1):
+    if andar != 13:
+        print(andar)
+
+print("While")
+# Usando loop while
+andar = 20
+while andar > 0:
+    if andar != 13:
+        print(andar)
+    andar -= 1
